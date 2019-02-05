cbe9c9217c9ae1537d026dde44dfd4b7fa07e3e9
mike2mars <mike2mars@hotmail.com>
2019-02-05 21:50:23 +0100

    commentaires

--- View/layout.view.php
+++ View/layout.view.php
@@ -14,7 +14,7 @@ $vcontent = new $content['class']();
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="content-type" content="text/html" />
+        <!--meta http-equiv="content-type" content="text/html" /-->
         <meta name="viewport" content="width=device-width" />
 
         <title><?=$content['title'];?></title>
@@ -28,16 +28,19 @@ $vcontent = new $content['class']();
             <?=$vheader->showHeader()?>
         </header>
 
-        <?php $vcontent->{$content['method']}($content['arg']);?>
+        <main>
+            <?php $vcontent->{$content['method']}($content['arg']);?>
+        </main>
 
         <script src="../Foundation/node_modules/jquery/dist/jquery.js"></script>
-        <script src="../Foundation/node_modules/what-input/dist/what-input.js"></script>
+        <!--script src="../Foundation/node_modules/what-input/dist/what-input.js"></script-->
         <script src="../Foundation/node_modules/foundation-sites/dist/js/foundation.js"></script>
         <script src="../Foundation/js/app.js"></script>
         <script src="../js/changeContent.js"></script>
-        <script src="../js/foundation.core.js"></script>
-        <script src="../js/foundation.tabs.js"></script>
-        <script src="../js/foundation.util.imageLoader.js"></script>
-        <script src="../js/foundation.util.keyboard.js"></script>
+        <!--script src="../js/foundation.core.js"></script-->
+        <!--script src="../js/foundation.tabs.js"></script-->
+        <!--script src="../js/foundation.util.imageLoader.js"></script-->
+        <!--script src="../js/foundation.util.keyboard.js"></script-->
+        <noscript>Votre navigateur n'accepte pas Javascript. Pour une expérience optimale, il est fortement conseillé d'activer cette option.</noscript>
     </body>
 </html>
