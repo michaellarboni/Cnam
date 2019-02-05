66280a019121e4a9621a637f9d1cb4c1a0ccd637
mike2mars <mike2mars@hotmail.com>
2019-02-05 21:33:09 +0100

    modif class foundation

--- Html/home.html.php
+++ Html/home.html.php
@@ -1,36 +1,7 @@
-<div class="grid-x grid-margin-x">
-    <div class="cell small-2">
-
-    </div>
-
-    <div class="cell auto">
-        <div class="">
+<div class="grid-x grid-padding-x align-center-middle text-center">
+        <div>
             <h2>Bienvenue sur le site de Cat Clinic</h2>
             <p>Spécialisée dans l'accueil et le soin des félins (Chat, Hyenes, Tigre du Bengale etc.)</p>
             <p>CAT CLINIC	vous présente son savoir faire.</p>
         </div>
-    </div>
-    <div class="cell small-2">
-
-    </div>
 </div>
-
-<div class="grid-x grid-padding-x">
-    <div class="cell small-3">
-
-    </div>
-
-    <div class="cell auto">
-        <div>
-            <h3>A faire</h3>
-            <ul>
-                <li>formulaire de contact</li>
-                <li>rendre privé le mode admin</li>
-                <li>ajout de fonctions pour modifier les fiches conseils</li>
-                <li>voir base de données non compatible server raspi</li>
-            </ul>
-        </div>
-    </div>
-
-</div>
-
