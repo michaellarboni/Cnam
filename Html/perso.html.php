e1b7c183ea44b273013a16b5a97d918f8ed98f51
mike2mars <mike2mars@hotmail.com>
2019-02-09 19:23:08 +0100

    mise a jour structure dossiers
    mise a jour structure dossiers

---
+++ Html/perso.html.php
@@ -0,0 +1,11 @@
+<div class="grid-x grid-padding-x align-center-middle ">
+    <div>
+        <h3>A faire</h3>
+        <ul>
+            <li>formulaire de contact</li>
+            <li>rendre privé le mode admin</li>
+            <li>ajout de fonctions pour modifier les fiches conseils</li>
+            <li>voir base de données non compatible server raspi</li>
+        </ul>
+    </div>
+</div>
