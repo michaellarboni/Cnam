07c1f260680ab2b30ecbc5588f9718beb98ad562
mike2mars <mike2mars@hotmail.com>
2019-02-23 11:23:50 +0100

    update home.html

--- Html/home.html.php
+++ Html/home.html.php
@@ -1,7 +1,7 @@
 <div class="grid-x grid-padding-x align-center-middle text-center">
         <div>
             <h2>Bienvenue sur le site de Cat Clinic</h2>
-            <p>Spécialisée dans l'accueil et le soin des félins (Chat, Hyenes, Tigre du Bengale etc.)</p>
-            <p>CAT CLINIC	vous présente son savoir faire.</p>
+            <h3>Spécialisée dans l'accueil et le soin des félins (Chat, Hyenes, Tigre du Bengale etc.)</h3>
+            <h3>CAT CLINIC	vous présente son savoir faire.</h3>
         </div>
 </div>
