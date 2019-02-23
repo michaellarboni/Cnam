3d1f06795d917857173cb46a19bfb66eda431c10
mike2mars <mike2mars@hotmail.com>
2019-02-23 11:19:23 +0100

    add Admin

---
+++ Html/admin.html.php
@@ -0,0 +1,6 @@
+<div class="grid-x grid-padding-x align-center-middle text-center">
+        <div>
+            <h2>Administration des Items</h2>
+            <h3>Cliquez sur un item pour le modifier ou cliquez sur "nouveau" pour ajouter</h3>
+        </div>
+</div>
---
+++ Html/admin_contenu.html.php
@@ -0,0 +1,6 @@
+<div class="grid-x grid-padding-x align-center-middle text-center">
+        <div>
+            <h2>Administration des Fiches</h2>
+            <p><strong>Cliquez sur un item pour modifier son contenu</strong></p>
+        </div>
+</div>
