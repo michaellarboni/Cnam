fcb93a832b20749a32362765f0cfa5fcc6929289
mike2mars <mike2mars@hotmail.com>
2019-02-23 11:43:24 +0100

    Update require.inc.php

--- Inc/require.inc.php
+++ Inc/require.inc.php
@@ -2,9 +2,10 @@
 
 // Constantes pour la Base de données
 define('DEBUG', true);
-define('DATABASE', 'mysql:host=localhost;dbname=cat_clinic');
+define('DATABASE', 'mysql:host=localhost;dbname=cat_clinic;charset=utf8');
 define('LOGIN', 'root');
-define('PASSWORD', 'makaveliofmars13');
+define('PASSWORD', 'MLarboni');
+define('CHARSET', 'utf8');
 
 spl_autoload_register('my_autoload');
 
@@ -22,6 +23,26 @@ function my_autoload($class)
     return;
 
 } // __autoload($class)
+
+function strip_xss(&$val)
+{
+    // Teste si $val est un tableau
+    if (is_array($val))
+    {
+        // Si $val est un tableau, on réapplique la fonction strip_xss()
+        array_walk($val, 'strip_xss');
+    }
+    else if (is_string($val))
+    {
+        // Si $val est une string, on filtre avec strip_tags()
+
+        //$val = strip_tags($val,'<p>');
+        $val = strip_tags($val);
+    }
+
+    return;
+
+} // strip_xss(&$val)
 
 function debug($Tab)
 {
