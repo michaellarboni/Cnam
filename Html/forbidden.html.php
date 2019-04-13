3341ca04a2caca84e5654abda489b0f76ef032bb
mike2mars <mike2mars@hotmail.com>
2019-04-13 12:48:06 +0200

    add page html

---
+++ Html/forbidden.html.php
@@ -0,0 +1,5 @@
+<div class="grid-x grid-padding-x align-center-middle text-center">
+    <div>
+        <h3>La page demandée est introuvable</h3>
+    </div>
+</div>
---
+++ Html/interdit.html.php
@@ -0,0 +1,5 @@
+<div class="grid-x grid-padding-x align-center-middle text-center">
+    <div>
+        <h3>Autorisation nécessaire !</h3>
+    </div>
+</div>
