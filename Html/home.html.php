4724acbd6cfdeff0ae586b2602bba961f8798030
mike2mars <mike2mars@hotmail.com>
2019-04-13 12:48:54 +0200

    indentation

--- Html/home.html.php
+++ Html/home.html.php
@@ -1,7 +1,6 @@
-<div class="grid-x grid-padding-x align-center-middle text-center">
+    <div class="grid-x grid-padding-x align-center-middle text-center">
         <div>
-            <h2>Bienvenue sur le site de Cat Clinic</h2>
             <h3>Spécialisée dans l'accueil et le soin des félins (Chat, Hyenes, Tigre du Bengale etc.)</h3>
             <h3>CAT CLINIC	vous présente son savoir faire.</h3>
         </div>
-</div>
+    </div>
