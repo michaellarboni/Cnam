55bfa13223893da00967ff9005c6724be64ffc66
mike2mars <mike2mars@hotmail.com>
2019-04-13 12:53:53 +0200

    delete files

--- Html/admin_contenu.html.php
+++
@@ -1,6 +0,0 @@
-<div class="grid-x grid-padding-x align-center-middle text-center">
-        <div>
-            <h2>Administration des Fiches</h2>
-            <p><strong>Cliquez sur un item pour modifier son contenu</strong></p>
-        </div>
-</div>
