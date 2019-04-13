6edf6ba5432a470100d21a9e18dd6e615d22837e
mike2mars <mike2mars@hotmail.com>
2019-04-13 12:54:38 +0200

    mise a jour VPages

--- View/VPages.view.php
+++ View/VPages.view.php
@@ -15,112 +15,75 @@ class VPages
      */
     public function showPage($_data)
     {
-        $li  = '' ;
-        $div = '' ;
-        $nouveau = isset($_SESSION['ADMIN_CONTENU']) ? '<li class="tabs-title"><a style="color: orange" href="../Php/index.php?EX=formContenu&amp;ID_ITEM='.$_GET['ID_ITEM'].'&amp;ITEM='.$_GET['ITEM'].'">Nouvelle fiche</a></li>':'';
+
+        $value ['ID_ITEM'] = isset($_GET['ID_ITEM']) ? $_GET['ID_ITEM'] : null ;
+        $mpages = new MPages();
+        $mpages ->SetValue($value);
+        //$data['ITEM'] = $mpages->SelectAll();
+//        $_data['ID_ITEM'] = $mpages->SelectAll();
+        //array_walk($data, 'strip_xss');
+
+//        global $aucun;
+
+        $li      = '';
+        $div     = '';
+        $aucun   = isset($_data[0]['ID_CONTENU']) ? '' : 'Aucun contenu disponible' ;
+        $nouveau = isset($_SESSION['ADMIN']) ? '<li class="tabs-title"><a class="admin" href="../Php/index.php?EX=form_contenu&amp;ID_ITEM='.$_REQUEST['ID_ITEM'].'&amp;ITEM='.$_REQUEST['ITEM'].'">Nouvelle fiche</a></li>' :'';
 
         // Boucle sur les tuples de la table contenus
-        foreach ($_data['ITEM'] as $val)
+        foreach ($_data as $val)
         {
-            $id_item    =  $val['ID_ITEM'];
-            $item       =  $val['ITEM'];
             $id_contenu = $val['ID_CONTENU'];
-            $titre      = '<h2>'.$val['TITRE'].'</h2>';
-            $texte      = '<p>'.$val['TEXTE'].'</p>';
-            $paragraphe = '<p>'.$val['PARAGRAPHE'].'</p>';
-	    $div       .= '<div class="tabs-panel is-active" id="panel'.$id_contenu.'">
-                            '.$titre.$texte.$paragraphe.'
+            $titre      = $val['TITRE'];
+            $texte      = $val['TEXTE'];
+            $paragraphe = $val['PARAGRAPHE'];
+            $classLi    = 'class="tabs-title"';
+            $classDiv    = 'is-active';
+            $div        .= '<div class="tabs-panel '.$classDiv.'" data-update-history="true" id="panel'.$id_contenu.'">
+                                <h2>'.$titre.'</h2>
+                                <p> '.$texte.'</p>
+                                <p> '.$paragraphe.'</p>
                            </div>';
 
-            if (isset($_SESSION['ADMIN_CONTENU']))
+            if (isset($_SESSION['ADMIN']))
             {
-                $li .= '<li class="tabs-title"><a href="../Php/index.php?EX=formContenu&amp;ID_CONTENU='.$id_contenu.'&amp;ID_ITEM='.$id_item.'&amp;ITEM='.$item.'">'.$val['TITRE'].'</a></li>' ;
-                $nouveau = '<li class="tabs-title"><a style="color: orange" href="../Php/index.php?EX=formContenu&amp;ID_ITEM='.$id_item.'&amp;ITEM='.$item.'">Nouvelle</a></li>';
+                if ((MODIFICATION & $_SESSION['AUTORISATION']) == MODIFICATION)
+                    {
+                        $ex = 'form_contenu&amp;ID_CONTENU='.$id_contenu.'';}
+                else
+                    {
+                        $ex= 'page&amp;ID_ITEM='.$_REQUEST['ID_ITEM'].'';
+                    }
+
+                $li .= '<li '.$classLi.'><a href="../Php/index.php?EX='.$ex.'&amp;ITEM='.$_REQUEST['ITEM'].'#'.$id_contenu.'">'.$titre.'</a></li>';
+
             }
-            else {$li .= '<li class="tabs-title"><a href="../Php/index.php?EX=page&amp;ID_CONTENU='.$id_contenu.'&amp;ID_ITEM='.$id_item.'">'.$val['TITRE'].'</a></li>' ;}}
+            else
+                {
+                    $li .= '<li '.$classLi.'><a href="#panel'.$id_contenu.'">'.$titre.'</a></li>' ;
+                }
+        }
 
         echo<<<HERE
 <div class="grid-container">
     <div class="grid-x grid-margin-x">
         <div class="cell medium-3">
-            <ul class="vertical tabs" data-tabs id="example-tabs">
+            <ul class="vertical tabs" data-tabs data-sticky data-options="marginTop:7;" id="example-tabs">
                $li
                $nouveau
+               $aucun
             </ul>
         </div>
         <div class="cell medium-9">
             <div class="tabs-content" data-tabs-content="example-tabs">
                 $div
+                <h2>$aucun</h2>
              </div>
         </div>
     </div>
 </div>
 HERE;
-    } // showPage
-
-    /**
-     * Affichage du formulaire
-     * @param array données du contenu
-     *
-     * @return
-     */
-    public function showForm($_data)
-    {
-        $id_item   = isset($_data['ID_ITEM']) ? $_data['ID_ITEM'] : '' ;
-
-        if (isset($_data['ID_CONTENU']))
-        {
-            $ex   	    = 'update_contenu&ID_CONTENU='.$_data['ID_CONTENU'];
-            $titre 	    = $_data['TITRE'];
-            $texte 	    = $_data['TEXTE'];
-	    $paragraphe     = $_data['PARAGRAPHE'];
-            $id_contenu = $_data['ID_CONTENU'];
-            $supprimer  = '<a href="../Php/index.php?EX=delete_contenu&amp;ID_CONTENU='.$_data['ID_CONTENU'].'"><button>Supprimer</button></a>';
-        }
-        else
-        {
-            $ex   	    = 'insert_contenu' ;
-            $titre 	    = '' ;
-            $texte 	    = '' ;
-	    $paragraphe     = '' ;
-            $id_contenu = '' ;
-            $supprimer  = '' ;
-        }
-
-        echo <<<HERE
-<div style="text-align: center">
-    <form action="../Php/index.php?EX=$ex" method="post">
-     <fieldset>
-      <legend style="color: white">Formulaire</legend>
-       
-       <div class="contactChamp">
-            <input id="titre" name="TITRE" value="$titre" size="25" required="required" maxlength="50" />
-            <label for="titre" id="champTitre" style="width: 20%;" class="erreurLab">Titre</label>
-        </div>
-
-        <div class="contactChamp">
-            <textarea id="texte" name="TEXTE" placeholder="Tapez votre texte ici, pour ajouter un paragraphe entourez le avec des balises <p>Comme ceci </p>" class="erreurLab">$texte</textarea>
-            <label for="texte" id="champTexte" style="width: 20%;"  class="erreurLab">Texte</label>
-        </div>
-
-	<div class="contactChamp">
-	     <textarea id="paragraphe" name="PARAGRAPHE" placeholder="Ecrivez du texte" class="erreurLab">$paragraphe</textarea>
-	     <label id="champParagraphe" style="width: 20%;" class="erreurLab">Paragraphe</label>
-	</div>
-         
-         <input id="id_item" name="ID_ITEM" value="$id_item" size="25" maxlength="100" type="hidden"/>       
-         <input id="id_contenu" name="ID_CONTENU" value="$id_contenu" size="25" maxlength="50" type="hidden"/>
-              
-     
-      <p class="submit">
-       <input type="submit" value="Ok" />
-      </p>
-     </fieldset>
-    </form>
-    $supprimer
-</div>
-HERE;
         return;
-    } // showForm
 
+    } // showPage
 } // Vpages
